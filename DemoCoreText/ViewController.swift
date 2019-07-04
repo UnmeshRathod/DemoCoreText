4e47f3b5cb1bb6632f28548f1f94c7cdd9d4bef3
Unmesh <unmesh@sensibol.com>
2019-07-04 15:50:29 +0530

    Initial Commit

---
+++ DemoCoreText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCoreText
+//
+//  Created by Unmesh Rathod on 04/07/19.
+//  Copyright © 2019 UnmeshRathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
